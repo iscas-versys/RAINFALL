2926af21ee608fccdbd015cf8c8c80194e843320
Lirongkang <715205884@qq.com>
2026-07-04 16:46:13 +0800

    Clean up imports in __init__.py
    Removed unused imports from __init__.py

--- prompt/__init__.py
+++ prompt/__init__.py
@@ -4,8 +4,6 @@ from .code_check_prompt import *
 from .counterexample_prompt import *
 from .rf_check_prompt import *
 from .c_initial_prompt_without_fewshot import *
-from .c_initial_prompt_ranking_gen import *
-from .c_initial_prompt_ranking_gen_without_few_shot import *
-from .counterexample_prompt_ranking_gen import *
+
 
 
